802a6ceed698fb408c8307e582d69f398ef06339
sammens <samuelmensah@aims.ac.za>
2020-11-04 10:35:47 +0200

    made corrections to config

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ def get_args():
                         help='Which data to use')
     parser.add_argument('--model', type=str,
                         help='which model to use')
-    parser.add_argument('--oversample', type=bool, default=False
+    parser.add_argument('--oversample', type=bool, default=False,
                         help='oversampling')
     
     args = parser.parse_args()
